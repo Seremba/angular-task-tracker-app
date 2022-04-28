698cb42f6a2622a2a87f8350d767d919b83fe8c2
Seremba Patrick <pserembae@gmail.com>
2022-04-28 17:14:19 +0300

    added the delete functionality to the task tracker app

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import Task from '../../Task';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,11 +8,17 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./tasks-item.component.css']
 })
 export class TasksItemComponent implements OnInit {
+
  @Input() task !: Task
+ @Output() onDeleteTask : EventEmitter<Task> = new EventEmitter();
+
  faTrash = faTrash;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+   
+  onDelete(task: any){
+    this.onDeleteTask.emit(task);
+  }
 }
