ce60b29fbfc2ef56e4304ec611cd2ca7c9393cbb
Seremba Patrick <pserembae@gmail.com>
2022-04-28 10:21:47 +0300

    add task service, created observables to the task tracker app

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {TaskService} from '../../services/task.service'
 import Task from '../../Task'
-import { TASKS } from '../../mock-tasks';
+
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +9,12 @@ import { TASKS } from '../../mock-tasks';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(private taskSerService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskSerService.getTasks().subscribe(tasks => this.tasks = tasks );
   }
 
 }
