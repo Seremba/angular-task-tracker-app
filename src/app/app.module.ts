290da9730fe744b16549904a37cde64a14b6a75c
Seremba Patrick <pserembae@gmail.com>
2022-05-02 19:16:31 +0300

    added the 'add task' functionality through the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule} from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
